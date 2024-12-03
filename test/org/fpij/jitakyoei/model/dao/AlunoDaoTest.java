132e73a7e9dbb6b76d16f343f4da4c96810ceff2
forgelucas <lucasforge1209@gmail.com>
2024-12-02 23:21:23 -0300

    Importando a assertnotnull

--- test/org/fpij/jitakyoei/model/dao/AlunoDaoTest.java
+++ test/org/fpij/jitakyoei/model/dao/AlunoDaoTest.java
@@ -14,6 +14,7 @@ import org.fpij.jitakyoei.util.DatabaseManager;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 public class AlunoDaoTest {
 	
